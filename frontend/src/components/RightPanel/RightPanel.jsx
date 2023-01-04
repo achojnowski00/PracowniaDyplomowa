b2c8fc11a1edd0dd21af206a9cfd5fd806f4eaee
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-01-04 14:18:49 +0100

    F3000 Komponent prawego panelu

---
+++ frontend/src/components/RightPanel/RightPanel.jsx
@@ -0,0 +1,11 @@
+import React, { useContext, useState, useEffect } from "react";
+import "./RightPanel.sass";
+export const RightPanel = () => {
+  return (
+    <>
+      <div className="RightPanel">
+        <h1>RightPanel</h1>
+      </div>
+    </>
+  );
+};
