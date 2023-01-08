a8f4243e80c0fe651b0543f6418d8c1206c68dab
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-01-08 02:45:45 +0100

    proba deploya 1

--- frontend/src/context/apiContext.jsx
+++ frontend/src/context/apiContext.jsx
@@ -4,7 +4,10 @@ import axios from "axios";
 export const ApiContext = createContext();
 
 export const ApiProvider = (props) => {
-  const BACKEND_URL = "http://192.168.1.17:8000";
+  // Change BACKEND_URL to your backend url
+  // const BACKEND_URL = "http://192.168.1.17:8000"; // PC
+  // const BACKEND_URL = "http://192.168.1.13:8000"; // Laptop
+  const BACKEND_URL = "https://homeorganizer.onrender.com";
 
   return (
     <ApiContext.Provider value={BACKEND_URL}>
