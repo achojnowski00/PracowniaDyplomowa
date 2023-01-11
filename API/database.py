a236d45b3842b5dd829dccdc065960ddd8a5c02e
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-01-11 21:17:59 +0100

    proba deploya

--- API/database.py
+++ API/database.py
@@ -4,10 +4,10 @@ import sqlalchemy.orm as _orm
 
 # DATABASE_URL = "mysql+mysqldb://root:@localhost/inzynierka"
 # DATABASE_URL = "mysql+mysqldb://root:@localhost/inzynierkaNEW"
-DATABASE_URL = "mysql+mysqldb://root:@localhost/homeorganizer"
+# DATABASE_URL = "mysql+mysqldb://root:@localhost/homeorganizer"
 # DATABASE_URL = "sqlite:///./database.db"
 # DATABASE_URL = "mysql+mysqldb://login:password@host/database"
-
+DATABASE_URL = "postgresql://root:78zp6NgNDtbCYSG2zDBFMcMueI00088Z@dpg-cet1cvhgp3jmgl7f5m10-a.frankfurt-postgres.render.com/inzynierka"
 
 
 engine = _sql.create_engine(DATABASE_URL)
