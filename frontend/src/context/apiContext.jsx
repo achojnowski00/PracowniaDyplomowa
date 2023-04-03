749812b62f80cee845e3f7a5a0c032344176948a
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-04-03 14:55:23 +0200

    nowy server deta deploy

--- frontend/src/context/apiContext.jsx
+++ frontend/src/context/apiContext.jsx
@@ -7,8 +7,9 @@ export const ApiProvider = (props) => {
   // Change BACKEND_URL to your backend url
   // const BACKEND_URL = "http://192.168.1.17:8000"; // PC
   // const BACKEND_URL = "http://192.168.1.13:8000"; // Laptop
-  const BACKEND_URL = "http://localhost:8000"; // Laptop
+  // const BACKEND_URL = "http://localhost:8000"; // Laptop
   // const BACKEND_URL = "https://homeorganizer.onrender.com";
+  const BACKEND_URL = "https://homeorgapi-1-l5534698.deta.app";
 
   return (
     <ApiContext.Provider value={BACKEND_URL}>
