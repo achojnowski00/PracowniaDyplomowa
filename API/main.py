8530abc3b61250784bfec47411cd89e251182c58
Aleksander Chojnowski <achojnowski00@gmail.com>
2023-04-03 15:11:44 +0200

    kolejna zmiana CORS

--- API/main.py
+++ API/main.py
@@ -21,12 +21,13 @@ app = _fastapi.FastAPI(
 )
 
 origins = [
-    "*",
+    "https://homeorganizer.netlify.app",
+    "http://localhost:3000",
 ]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
